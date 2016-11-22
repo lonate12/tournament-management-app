b1cf92fa068388dd5bf86423484863eacd95175f
Luis Rene Onate <lonate12@gmail.com>
2016-11-22 11:17:42 -0500

    Small http to https tweak.

--- app/scripts/components/teamView.jsx
+++ app/scripts/components/teamView.jsx
@@ -39,7 +39,7 @@ var TeamViewContainer = React.createClass({
         <div className="row">
           <div className="col-md-4">
             <h2 className="team-name">{currentTeam.get('name')}</h2>
-            <img src={currentTeam.get('logo') ? currentTeam.get('logo') : '../../dist/images/default-logo.png'} />
+            <img src={currentTeam.get('logo') ? currentTeam.get('logo') : '../../images/default-logo.png'} />
           </div>
           <div className="col-md-8">
 
