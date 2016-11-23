7c6dc5d5b67f2ee270429acf6d64f073e10ade67
Luis Rene Onate <lonate12@gmail.com>
2016-11-23 15:40:56 -0500

    Updated add capabilities for admin. Proceeding to update the delete capability.

--- app/scripts/components/adminTournamentDash.jsx
+++ app/scripts/components/adminTournamentDash.jsx
@@ -8,6 +8,129 @@ var LocationModels = require('../models/location.js'), LocationModel = LocationM
 var TournamentDashTemplate = require('../display/tournamentDashTemplate.jsx').TournamentDashTemplate;
 var Modal = require('react-modal');
 
+var TeamUpdateModal = React.createClass({
+  getInitialState: function(){
+    return {
+      selectedTeam: this.props.selectedTeam,
+      modalIsOpen: false,
+    }
+  },componentWillReceiveProps: function(nextProps){
+    this.setState({selectedTeam: nextProps.selectedTeam});
+
+    if(nextProps.selectedTeam.get('objectId')){
+      this.openModal();
+    }
+  },
+  openModal: function(){
+    this.setState({modalIsOpen: true});
+  },
+  closeModal: function(){
+    this.setState({modalIsOpen: false});
+  },
+  handleChange: function(e){
+    var selectedTeam = this.state.selectedTeam;
+
+    selectedTeam.set(e.target.name, e.target.checked);
+  },
+  saveChanges: function(e){
+    e.preventDefault();
+    var self = this;
+
+    this.state.selectedTeam.save().then(function(){
+      self.props.updateSelected(self.state.selectedTeam);
+      self.closeModal();
+    });
+  },
+  render: function(){
+    var team = this.props.selectedTeam;
+
+    return(
+      <Modal
+        isOpen={this.state.modalIsOpen}
+        onRequestClose={this.closeModal} >
+        <h2>{team.get('name')}</h2>
+        <form onSubmit={this.saveChanges}>
+          <div className="form-group">
+            <label htmlFor="have_paid">
+              Paid <input onChange={this.handleChange} defaultChecked={team.get('have_paid')} type="checkbox" name="have_paid"/>
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="have_waiver">
+              Submitted Waiver <input onChange={this.handleChange} defaultChecked={team.get('have_waiver')} type="checkbox" name="have_waiver"/>
+            </label>
+          </div>
+          <button type="submit" className="btn btn-success">Save Changes</button>
+        </form>
+      </Modal>
+    );
+  }
+});
+
+
+var TeamsComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      teams: this.props.teams,
+      modalIsOpen: false,
+      selectedTeam: new Team()
+    }
+  },
+  openModal: function(){
+    this.setState({modalIsOpen: true});
+  },
+  closeModal: function(){
+    this.setState({modalIsOpen: false});
+  },
+  componentWillReceiveProps: function(nextProps){
+    this.setState({games: nextProps.teams});
+  },
+  selectTeam: function(e){
+    e.preventDefault();
+    var selectedTeam = this.state.selectedTeam, self = this;
+
+    selectedTeam.set({objectId: e.target.id});
+    selectedTeam.fetch().then(function(){
+      self.setState({
+        selectedTeam: selectedTeam
+      });
+    });
+  },
+  render: function(){
+    var teams = this.state.teams, teamTableData, self = this;
+
+    if (teams) {
+      teamTableData = teams.map(function(team){
+        return(
+          <tr key={team.get('objectId')}>
+            <td onClick={self.selectTeam} id={team.get('objectId')}>{team.get('name')}</td>
+            <td className={team.get('have_paid') ? 'success' : 'danger'}>{team.get('have_paid') ? 'Have Paid' : 'Have Not Paid'}</td>
+            <td className={team.get('have_waiver') ? 'success' : 'danger'}>{team.get('have_waiver') ? 'Have Turned in Waiver' : 'Have not turned in Waiver'}</td>
+          </tr>
+        );
+      });
+    }
+
+    return(
+      <div className="col-sm-12">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Payment</th>
+              <th>Waiver</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teamTableData ? teamTableData : <tr colspan="3"><td>Teams Loading...</td></tr>}
+          </tbody>
+        </table>
+        <TeamUpdateModal tournamentId={this.props.tournamentId} updateSelected={this.props.updateSelected} selectedTeam={this.state.selectedTeam}/>
+      </div>
+    );
+  }
+});
+
 var AddGameModal = React.createClass({
   getInitialState: function(){
     return {
@@ -61,11 +184,16 @@ var AddLocationModal = React.createClass({
     this.setState({modalIsOpen: false});
   },
   handleChange: function(e){
+    var target = e.target;
 
+    this.state.newLocation.set(e.target.name, e.target.value);
   },
   addLocation: function(e){
     e.preventDefault();
 
+    this.setState({newLocation: this.state.newLocation});
+    this.props.addLocation(this.state.newLocation);
+    this.props.closeModal();
   },
   render: function(){
     return(
@@ -73,8 +201,78 @@ var AddLocationModal = React.createClass({
         isOpen={this.state.modalIsOpen}
         onRequestClose={this.closeModal} >
         <form onSubmit={this.addLocation}>
-          <label htmlFor="test">Test</label>
-          <input type="text" name="test" placeholder="Test"/>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="name" placeholder="Name of Location" required="required"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="street_address">Street Address</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="street_address" placeholder="Street Address" required="required"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="city">City</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="city" placeholder="City" required="required"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="state">State</label>
+            <select onChange={this.handleChange} className="form-control" name="state" id="state" required="required">
+              <option value="AL">Alabama</option>
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="CA">California</option>
+              <option value="CO">Colorado</option>
+              <option value="CT">Connecticut</option>
+              <option value="DE">Delaware</option>
+              <option value="DC">District Of Columbia</option>
+              <option value="FL">Florida</option>
+              <option value="GA">Georgia</option>
+              <option value="HI">Hawaii</option>
+              <option value="ID">Idaho</option>
+              <option value="IL">Illinois</option>
+              <option value="IN">Indiana</option>
+              <option value="IA">Iowa</option>
+              <option value="KS">Kansas</option>
+              <option value="KY">Kentucky</option>
+              <option value="LA">Louisiana</option>
+              <option value="ME">Maine</option>
+              <option value="MD">Maryland</option>
+              <option value="MA">Massachusetts</option>
+              <option value="MI">Michigan</option>
+              <option value="MN">Minnesota</option>
+              <option value="MS">Mississippi</option>
+              <option value="MO">Missouri</option>
+              <option value="MT">Montana</option>
+              <option value="NE">Nebraska</option>
+              <option value="NV">Nevada</option>
+              <option value="NH">New Hampshire</option>
+              <option value="NJ">New Jersey</option>
+              <option value="NM">New Mexico</option>
+              <option value="NY">New York</option>
+              <option value="NC">North Carolina</option>
+              <option value="ND">North Dakota</option>
+              <option value="OH">Ohio</option>
+              <option value="OK">Oklahoma</option>
+              <option value="OR">Oregon</option>
+              <option value="PA">Pennsylvania</option>
+              <option value="RI">Rhode Island</option>
+              <option value="SC">South Carolina</option>
+              <option value="SD">South Dakota</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="zip_code">Zip Code</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="zip_code" placeholder="Zip Code" required="required"/>
+          </div>
           <button type="submit" className="btn btn-success">Add Location</button>
         </form>
       </Modal>
@@ -153,13 +351,20 @@ var LocationComponent = React.createClass({
   openModal: function(){
     this.setState({modalIsOpen: true});
   },
+  closeModal: function(closeModal){
+    this.setState({modalIsOpen: false});
+  },
+  addLocation: function(newLocation){
+    this.setState({modalIsOpen: false});
+    this.props.addLocation(newLocation);
+  },
   render: function(){
     var locations = this.state.locations, locationsList;
 
     if (locations) {
       locationsList = locations.map(function(location){
         return(
-          <tr key={location.get('objectId')}>
+          <tr key={location.cid}>
             <td>{location.get('name')}</td>
             <td>{location.get('street_address')}</td>
             <td>{location.get('city')}</td>
@@ -191,67 +396,8 @@ var LocationComponent = React.createClass({
             </table>
           </div>
         </div>
-        <AddLocationModal modalIsOpen={this.state.modalIsOpen} addLocation={this.props.addGame}/>
+        <AddLocationModal closeModal={this.closeModal} modalIsOpen={this.state.modalIsOpen} addLocation={this.props.addLocation}/>
       </div>
-    );
-  }
-});
-
-var TeamUpdateModal = React.createClass({
-  getInitialState: function(){
-    return {
-      selectedTeam: this.props.selectedTeam,
-      modalIsOpen: false,
-    }
-  },componentWillReceiveProps: function(nextProps){
-    this.setState({selectedTeam: nextProps.selectedTeam});
-
-    if(nextProps.selectedTeam.get('objectId')){
-      this.openModal();
-    }
-  },
-  openModal: function(){
-    this.setState({modalIsOpen: true});
-  },
-  closeModal: function(){
-    this.setState({modalIsOpen: false});
-  },
-  handleChange: function(e){
-    var selectedTeam = this.state.selectedTeam;
-
-    selectedTeam.set(e.target.name, e.target.checked);
-  },
-  saveChanges: function(e){
-    e.preventDefault();
-    var self = this;
-
-    this.state.selectedTeam.save().then(function(){
-      self.props.updateSelected(self.state.selectedTeam);
-      self.closeModal();
-    });
-  },
-  render: function(){
-    var team = this.props.selectedTeam;
-
-    return(
-      <Modal
-        isOpen={this.state.modalIsOpen}
-        onRequestClose={this.closeModal} >
-        <h2>{team.get('name')}</h2>
-        <form onSubmit={this.saveChanges}>
-          <div className="form-group">
-            <label htmlFor="have_paid">
-              Paid <input onChange={this.handleChange} defaultChecked={team.get('have_paid')} type="checkbox" name="have_paid"/>
-            </label>
-          </div>
-          <div className="form-group">
-            <label htmlFor="have_waiver">
-              Submitted Waiver <input onChange={this.handleChange} defaultChecked={team.get('have_waiver')} type="checkbox" name="have_waiver"/>
-            </label>
-          </div>
-          <button type="submit" className="btn btn-success">Save Changes</button>
-        </form>
-      </Modal>
     );
   }
 });
@@ -295,17 +441,6 @@ var AdminTournamentDash = React.createClass({
       self.setState({locations: locations});
     });
   },
-  selectTeam: function(e){
-    e.preventDefault();
-    var selectedTeam = this.state.selectedTeam, self = this;
-
-    selectedTeam.set({objectId: e.target.id});
-    selectedTeam.fetch().then(function(){
-      self.setState({
-        selectedTeam: selectedTeam
-      });
-    });
-  },
   updateSelected: function(selectedTeam){
     var teams = this.state.teams, self = this;
     teams.set(selectedTeam, {remove: false});
@@ -317,45 +452,25 @@ var AdminTournamentDash = React.createClass({
     this.setState({games: games});
   },
   addLocation: function(newLocation){
-    var locations = this.state.locations;
+    var locations = this.state.locations
+    , tournamentPointer = newLocation.toPointer('Tournaments', this.props.tournamentId);
+    newLocation.set({tournament: tournamentPointer});
     locations.create(newLocation);
     this.setState({locations: locations});
   },
   render: function(){
-    var teams = this.state.teams, teamTableData, self = this;
-
-    teamTableData = teams.map(function(team){
-      return(
-        <tr key={team.get('objectId')}>
-          <td onClick={self.selectTeam} id={team.get('objectId')}>{team.get('name')}</td>
-          <td className={team.get('have_paid') ? 'success' : 'danger'}>{team.get('have_paid') ? 'Have Paid' : 'Have Not Paid'}</td>
-          <td className={team.get('have_waiver') ? 'success' : 'danger'}>{team.get('have_waiver') ? 'Have Turned in Waiver' : 'Have not turned in Waiver'}</td>
-        </tr>
-      );
-    });
-
     return(
       <TournamentDashTemplate tournament={this.state.tournament}>
         <h1>AdminTournamentDash</h1>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Payment</th>
-              <th>Waiver</th>
-            </tr>
-          </thead>
-          <tbody>
-            {teamTableData}
-          </tbody>
-        </table>
+        <div className="row">
+          <TeamsComponent teams={this.state.teams} updateSelected={this.updateSelected}/>
+        </div>
         <div className="row">
           <ScheduleComponent games={this.state.games} addGame={this.addGame}/>
         </div>
         <div className="row">
-          <LocationComponent locations={this.state.locations}/>
+          <LocationComponent locations={this.state.locations} addLocation={this.addLocation}/>
         </div>
-        <TeamUpdateModal tournamentId={this.props.tournamentId} updateSelected={this.updateSelected} selectedTeam={this.state.selectedTeam}/>
       </TournamentDashTemplate>
     );
   }
